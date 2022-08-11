f89d38a29e9206ab8070bf64114fdd556c8afab1
Jacob Phillips <jacob.phillips8905@gmail.com>
2022-08-10 21:09:58 -0400

    rm pages workflow

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ const App = () => {
                     <Route path="/research" element={<Research />} />
                     <Route path="/work" element={<Work />} />
                     <Route path="/contact" element={<Contact />} />
-                    {/* <Route path="/jacobdphillips.com" element={<Home />} /> */}
                 </Routes>
             </Suspense>
         </Router>
