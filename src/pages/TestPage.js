e81539eb9e0fdf1f37b1dcdfecd32d46ab6e1895
Jacob Phillips <jacob.phillips8905@gmail.com>
2022-08-17 13:10:57 -0400

    test page

--- src/pages/TestPage.js
+++ src/pages/TestPage.js
@@ -1,30 +1,79 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
+const testData = [
+  {
+    content: 'test1',
+  },
+  {
+    content: 'test2',
+  },
+  {
+    content: 'test3',
+  },
+];
+
+const TestContainer = ({ data }) => (
+  <p>{data.content}</p>
+);
+
+TestContainer.propTypes = {
+  data: PropTypes.shape({
+    content: PropTypes.string,
+  }).isRequired,
+};
+
+const useStyles = makeStyles({
+  testDiv: {
+    color: 'red',
     '& p': {
-      margin: 0,
-      '&$title': {
-        margin: '0 0 16px',
-      },
+      color: 'blue',
     },
   },
-  title: {},
-}));
+  testP: {
+    color: 'green',
+  },
+  testUL: {
+    color: 'black',
+    background: 'lightblue',
+    listStyleType: 'square',
+    width: '300px',
+    border: '15px solid green',
+    padding: '50px',
+    margin: '10px 10px 100px 10px',
+    '& li': {
+      background: 'lightgreen',
+      margin: '5px',
+    },
+  },
+});
 
 const TestPage = () => {
-  const classes = useStyles();
+  const styles = useStyles();
   return (
-    <div className="TestPage">
-      <p>This paragraph isnt affected.</p>
-      <p>This paragraph isnt affected.</p>
-      <div className={classes.root}>
-        <p className={classes.title}>My title</p>
-        <p>A paragraph</p>
-        <p>Another paragraph</p>
+    <div>
+      <p className={styles.testP}> p </p>
+      <div className={styles.testDiv}>
+        Test Page Div
+        <p> hello </p>
+        <p> hello2 </p>
       </div>
+      <div>
+        Test Page Div 2
+        <p> hello again </p>
+        <p> hello2 again </p>
+      </div>
+      <ul className={styles.testUL}>
+        {/* {testData.map((data) => (
+          <TestContainer data={data} key={data.data} />
+        ))} */}
+        {testData.map((data) => (
+          <li>{data.content}</li>
+        ))}
+      </ul>
     </div>
+
   );
 };
 
